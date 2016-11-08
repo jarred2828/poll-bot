a67dcb03e83bd13590ab8becad2425c331dd7c76
Justin Lee <j.lee1205@hotmail.com>
2016-11-08 21:03:30 +0800

    detail page pie chart

--- client/app/components/detail/detail.controller.js
+++ client/app/components/detail/detail.controller.js
@@ -1,7 +1,36 @@
+import angular from 'angular';
+import Chart from 'chart.js';
+
+Chart.defaults.global.legend.display = false;
+
 class DetailController {
   static $inject = [];
   constructor() {
     this.name = 'detail';
+    this.dateInfo = {
+      start: new Date(this.pollInfo.start),
+      end: new Date(this.pollInfo.end)
+    };
+    this.renderChart();
+  }
+
+  renderChart() {
+    let ctx = document.getElementById('pie-chart').getContext('2d');
+    ctx.canvas.width = 150;
+    ctx.canvas.height = 150;
+    let pieChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: ['Answer1', 'Answer2', 'Answer3'],
+        datasets: [
+          {
+            data: [300, 50, 150],
+            backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
+            hoverBackgroundColor: ['#ff6384', '#36a2eb', '#ffce56']
+          }
+        ]
+      }
+    });
   }
 }
 
